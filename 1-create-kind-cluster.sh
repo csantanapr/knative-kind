c5d715f586386ef5955333a7deb56c4d7e542cb2
Carlos Santana <csantanapr@apache.org>
2019-11-07 12:55:24 -0500

    ignore error on delete cluster

--- 1-create-kind-cluster.sh
+++ 1-create-kind-cluster.sh
@@ -4,7 +4,9 @@ set -xeu
 
 CLUSTER_NAME=${CLUSTER_NAME:=knative}
 
+set +e
 kind delete cluster --name ${CLUSTER_NAME}
+set -e
 
 kind create cluster --image kindest/node:v1.16.1 --name ${CLUSTER_NAME} --config kind-config.yaml
 
