9372c99daea1f25cb5d168f38e321e4faa3661be
Omer B <obensaadon@vmware.com>
2021-04-08 22:27:06 -0400

    Update 02-serving.sh
    Removing samples, adding "success" message

--- 02-serving.sh
+++ 02-serving.sh
@@ -63,23 +63,4 @@ spec:
       targetPort: 8080
 EOF
 
-cat <<EOF | kubectl apply -f -
-apiVersion: serving.knative.dev/v1
-kind: Service
-metadata:
-  name: hello
-spec:
-  template:
-    spec:
-      containers:
-        - image: gcr.io/knative-samples/helloworld-go
-          ports:
-            - containerPort: 8080
-          env:
-            - name: TARGET
-              value: "Knative"
-EOF
-kubectl wait ksvc hello --all --timeout=-1s --for=condition=Ready
-SERVICE_URL=$(kubectl get ksvc hello -o jsonpath='{.status.url}')
-echo "The Knative Service hello endpoint is $SERVICE_URL"
-curl $SERVICE_URL
+printf "Knative Serving version $KNATIVE_VERSION successfully installed!\n"
