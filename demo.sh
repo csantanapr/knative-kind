4285e6420bc70df0f405316f01a19fc09fb0641f
Carlos Santana <csantana23@gmail.com>
2021-04-12 13:05:15 -0400

    Demo script refact (#25)
    * refactor demo with install script

--- demo.sh
+++ demo.sh
@@ -2,15 +2,15 @@
 
 set -eo pipefail
 
-echo -e "\033[0;92m 🍿 Knative starting... \033[0m"
+echo -e "\033[0;92m ✅ Checking dependencies... \033[0m"
+echo -e "🍿 Installing Knative Serving and Eventing ... \033[0m"
 STARTTIME=$(date +%s)
-curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/01-kind.sh | bash
-curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/02-serving.sh | bash
+curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/install.sh | bash
+echo -e "🕹 Installing Knative Samples Apps... \033[0m"
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/03-serving-samples.sh | bash
-curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/04-eventing.sh | bash
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/05-eventing-samples.sh | bash
 DURATION=$(($(date +%s) - $STARTTIME))
 echo "kubectl get ksvc,broker,trigger"
 kubectl -n default get ksvc,broker,trigger
-echo -e "\033[0;92m 🚀 Knative setup with samples took: $(($DURATION / 60))m$(($DURATION % 60))s \033[0m"
+echo -e "\033[0;92m 🚀 Knative install with samples took: $(($DURATION / 60))m$(($DURATION % 60))s \033[0m"
 echo -e "\033[0;92m 🎉 Now have some fun with Serverless and Event Driven Apps \033[0m"
