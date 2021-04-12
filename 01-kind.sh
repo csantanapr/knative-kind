6ada2ceaec38a58d70b7aff30863d96af0b3b70d
Carlos Santana <csantana23@gmail.com>
2021-04-12 13:39:30 -0400

    add brew-upgrade cmd

--- 01-kind.sh
+++ 01-kind.sh
@@ -8,6 +8,7 @@ K8S_VERSION=${k8sVersion:-v1.20.2@sha256:15d3b5c4f521a84896ed1ead1b14e4774d02202
 echo "KinD version is ${kindVersion}"
 if [[ ! $kindVersion =~ "v0.10." ]]; then
   echo "WARNING: Please make sure you are using KinD version v0.10.x, download from https://github.com/kubernetes-sigs/kind/releases"
+  echo "For example if using brew, run: brew upgrade kind"
   read -p "Do you want to continue on your own risk? Y/n: " REPLYKIND </dev/tty
   if [ "$REPLYKIND" == "Y" ] || [ "$REPLYKIND" == "y" ] || [ -z "$REPLYKIND" ]; then
     echo "You are very brave..."
