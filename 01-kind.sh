e216f5879179d12f006ab3b579950c29737ee4bd
Omer B <obensaadon@vmware.com>
2021-04-12 11:56:42 -0400

    Update 01-kind.sh
    Can we run this on .7? I couldn't finish a user interview today as the user had kind installed but could not update it...

--- 01-kind.sh
+++ 01-kind.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 kindVersion=$(kind version);
 K8S_VERSION=${k8sVersion:-v1.20.2@sha256:15d3b5c4f521a84896ed1ead1b14e4774d02202d5c65ab68f30eeaf310a3b1a7}
 
-if [[ $kindVersion =~ "v0.10." ]]
+if [[ $kindVersion =~ "v0.7." ]]
 then
    echo "KinD version is ${kindVersion}"
 else
-  echo "Please make sure you are using KinD v0.10.x, download from https://github.com/kubernetes-sigs/kind/releases"
+  echo "Please make sure you are using KinD v0.7.x, download from https://github.com/kubernetes-sigs/kind/releases"
   exit 1
 fi
 
