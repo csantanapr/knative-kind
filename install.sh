ea80b6e36137c27202b8b08951cb394bd2ef7f6f
Omer B <obensaadon@vmware.com>
2021-04-12 12:35:45 -0400

    Update install.sh (#22)
    user found "Knative Starting" before dependency check to be confusing... 
    
    Added some other echos

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 set -eo pipefail
 
-echo -e "\033[0;92m 🍿 Knative starting... \033[0m"
+echo -e "\033[0;92m ✅ Checking dependencies... \033[0m"
 STARTTIME=$(date +%s)
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/01-kind.sh | bash
+echo -e "🍿 Installing Knative Serving... \033[0m"
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/02-serving.sh | bash
+echo -e "🔥 Installing Knative Eventing... \033[0m"
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/04-eventing.sh | bash
 DURATION=$(($(date +%s) - $STARTTIME))
 echo "kubectl get ksvc,broker,trigger"
