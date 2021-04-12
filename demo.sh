6be31ee54a51bc35ce0ad0d0d887d0b13dae637b
Carlos Santana <csantana23@gmail.com>
2021-04-12 12:52:01 -0400

    update emoji for sample apps

--- demo.sh
+++ demo.sh
@@ -6,7 +6,7 @@ echo -e "\033[0;92m ✅ Checking dependencies... \033[0m"
 STARTTIME=$(date +%s)
 echo -e "🍿 Installing Knative Serving and Eventing ... \033[0m"
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/install.sh | bash
-echo -e "🔥 Installing Knative Samples Apps... \033[0m"
+echo -e "🕹 Installing Knative Samples Apps... \033[0m"
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/03-serving-samples.sh | bash
 curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/05-eventing-samples.sh | bash
 DURATION=$(($(date +%s) - $STARTTIME))
