d6dcc3332cb92229a08e31d7ed3cf54581396519
Carlos Santana <csantana23@gmail.com>
2021-05-24 14:11:10 -0400

    Add contour
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

---
+++ 02-kourier.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -u
+
+KNATIVE_NET_KOURIER_VERSION=${KNATIVE_NET_KOURIER_VERSION:-0.23.0}
+
+## INSTALL KOURIER
+n=0
+until [ $n -ge 2 ]; do
+  kubectl apply -f https://github.com/knative/net-kourier/releases/download/v$KNATIVE_NET_KOURIER_VERSION/kourier.yaml > /dev/null && break
+  n=$[$n+1]
+  sleep 5
+done
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kourier-system > /dev/null
+kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving > /dev/null
+
+# Configure Knative to use this ingress
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
+
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: kourier-ingress
+  namespace: kourier-system
+  labels:
+    networking.knative.dev/ingress-provider: kourier
+spec:
+  type: NodePort
+  selector:
+    app: 3scale-kourier-gateway
+  ports:
+    - name: http2
+      nodePort: 31080
+      port: 80
+      targetPort: 8080
+EOF
--- 02-serving.sh
+++ 02-serving.sh
@@ -9,58 +9,24 @@ INGRESS_HOST="127.0.0.1"
 KNATIVE_DOMAIN=$INGRESS_HOST.nip.io
 
 n=0
+set +e
 until [ $n -ge 2 ]; do
   kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-crds.yaml > /dev/null && break
   n=$[$n+1]
   sleep 5
 done
+set -e
 kubectl wait --for=condition=Established --all crd > /dev/null
 
 n=0
+set +e
 until [ $n -ge 2 ]; do
   kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE_VERSION/serving-core.yaml > /dev/null && break
   n=$[$n+1]
   sleep 5
 done
+set -e
 kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving > /dev/null
 
-n=0
-until [ $n -ge 2 ]; do
-  kubectl apply -f https://github.com/knative/net-kourier/releases/download/v$KNATIVE_NET_KOURIER_VERSION/kourier.yaml > /dev/null && break
-  n=$[$n+1]
-  sleep 5
-done
-kubectl wait --for=condition=Established --all crd > /dev/null
-
-kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kourier-system > /dev/null
-# deployment for net-kourier gets deployed to namespace knative-serving
-kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-serving > /dev/null
-
-kubectl patch configmap/config-network \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
-
-
+# Setup Knative DOMAIN DNS
 kubectl patch configmap -n knative-serving config-domain -p "{\"data\": {\"$KNATIVE_DOMAIN\": \"\"}}"
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: kourier-ingress
-  namespace: kourier-system
-  labels:
-    networking.knative.dev/ingress-provider: kourier
-spec:
-  type: NodePort
-  selector:
-    app: 3scale-kourier-gateway
-  ports:
-    - name: http2
-      nodePort: 31080
-      port: 80
-      targetPort: 8080
-EOF
-
-kubectl wait deployment --all --timeout=-1s --for=condition=Available -n kourier-system > /dev/null
