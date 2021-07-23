421554da46e8280dc2ae6da7611a77ec8fb1ca10
Carlos Santana <csantana23@gmail.com>
2021-07-22 22:52:31 -0400

    show labels
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- 04-eventing.sh
+++ 04-eventing.sh
@@ -46,7 +46,9 @@ until [ $n -ge 2 ]; do
 done
 #kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing
 sleep 30
-kubectl get pods -A
+kubectl get pods -A --show-labels
+#kubectl describe pod -n knative-eventing
+
 
 kubectl apply -f - <<EOF
 apiVersion: eventing.knative.dev/v1
